daf008f6c7537bef7a95d1d9bb06bde01dc7a160
Destinie-T <dtran172940@gmail.com>
2023-02-03 09:19:11 -0800

    proto 3 day 3
    things get destroyed, continous bg, things stop when you hit something

--- P1 Prototype 3/Assets/Scripts/PlayerController.cs
+++ P1 Prototype 3/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
 
     public bool isOnGround = true;
 
+    public bool gameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +33,13 @@ public class PlayerController : MonoBehaviour
     
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
     }
 }
