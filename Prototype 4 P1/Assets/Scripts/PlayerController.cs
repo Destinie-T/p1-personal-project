d72fb1e5e4fb070297bb3d45e45892bf6000c491
Destinie-T <dtran172940@gmail.com>
2023-03-21 09:16:09 -0700

    4.3
    finished
    idk why this is in p1 personal project

--- Prototype 4 P1/Assets/Scripts/PlayerController.cs
+++ Prototype 4 P1/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,12 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private GameObject focalPoint;
 
+    public GameObject powerupIndicator;
+
     public float speed = 5.0f;
+    private float powerupStrength = 10.0f;
+
+    public bool hasPowerup = false;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +26,41 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(other.gameObject);
+
+            powerupIndicator.gameObject.SetActive(true);
+
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter (Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+        }
+    }
+
 }
